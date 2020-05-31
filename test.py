e51c2f2329d30d2ba4b5e7c6c02bdc04431a6982
mbut17 <43580269+mbut17@users.noreply.github.com>
2020-05-31 22:02:20 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,20 +7,47 @@ class MyTestCase(unittest.TestCase):
             main.app.testing = True
             self.app = main.app.test_client()
         
-        def testint(self):
-            fed =  self.app.get('/sub?A=4&B=10')
-            self.assertEqual(b'-6.0', fed.data)
-            self.assertNotEqual(b'6.0',fed.data)
-        def testfloat(self):
-            fed = self.app.get('/sub?A=3.5&B=7.5')
-            self.assertEqual(b'-4.0', fed.data)
-            self.assertNotEqual(b'-1',fed.data)
-        def testfrac(self):
-            fed =  self.app.get('/sub?A=7/5&B=4/3')
-            self.assertEqual(b'-0.067', fed.data)
-        def testneg(self):
-            fed =  self.app.get('/sub?A=2.3&B=-3.3')
-            self.assertEqual(b'5.6', fed.data)
+       def test_sub1(self):
+		#case 1, A is n integer B is an integer
+		solution = self.app.get('/sub?A=6&B=2')
+		self.assertEqual(b'4.0', solution.data)
+
+	def test_sub2(self):
+
+		#case 2, A is rational number and B is rational number p/q form
+		solution = self.app.get('/sub?A=3/2&B=1/2')
+		self.assertEqual(b'1.0', solution.data)
+
+	def test_sub3(self):
+
+		#case 3, A is a float and B is a float
+		solution = self.app.get('/sub?A=10.2&B=1.002')
+		self.assertEqual(b'9.198', solution.data)
+
+	def test_sub4(self):
+
+		#case 4, when A is float and B is integer
+		solution = self.app.get('/sub?A=22.222&B=2')
+		self.assertEqual(b'20.222', solution.data)
+
+	def test_sub5(self):
+
+		#case 5, when A is integer and B is float
+		solution = self.app.get('/sub?A=12&B=1.111')
+		self.assertEqual(b'10.889', solution.data)
+
+	def test_sub6(self):
+
+		#case 6, when A is fraction p/q and B is an integer
+		solution = self.app.get('/sub?A=1/2&B=12')
+		self.assertEqual(b'-11.5', solution.data)
+
+	def test_sub7(self):
+
+		#case 7, when A is an integer and B is a fraction p/q
+		solution = self.app.get('/sub?A=3&B=2/10')
+		self.assertEqual(b'2.8', solution.data)
+)
         
 
 if __name__ == '__main__':
